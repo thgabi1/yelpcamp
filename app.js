e0eb6c49271908044baa52da5bd0d8fd0da1c9eb
Gabriella Toth <thgabi1@gmail.com>
2023-11-13 20:39:57 +0100

    add sample data

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
